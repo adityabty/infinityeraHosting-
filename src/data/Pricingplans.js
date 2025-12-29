ae04b2a90197a04c4715e9589e0153a1b558c8db
Xmusic <satyabhamabehera5931@gmail.com>
2025-12-29 09:48:09 +0530

    Delete src directory

--- src/data/Pricingplans.js
+++
@@ -1,5 +0,0 @@
-export const pricingPlans = [
-  { name: 'Starter', credits: 100, price: 99, duration: '30 days' },
-  { name: 'Pro', credits: 250, price: 249, duration: '30 days', popular: true },
-  { name: 'Enterprise', credits: 500, price: 449, duration: '30 days' }
-];
